d8510c580dedc3877ab5b19b043cdd4c1617a4fb
Осипов Кирилл Андреевич <202113@mck-ktits.ru>
2023-11-03 12:19:49 +0300

    Добавьте файлы проекта.

---
+++ StudentExam/db/DBConn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentExam.db
+{
+    public class DBConn
+    {
+        public static SEntities SEnt = new SEntities();
+    }
+}
---
+++ StudentExam/pages/LoginPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using StudentExam.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentExam.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthTBx_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTBx.Text.Trim();
+            string password = PasswordTBx.Password.Trim();
+
+            int errCounter = 0;
+            if (login.Length == 0 || password.Length == 0)
+            {
+                errCounter++;
+                ErrorTBk.Text = "Введите данные!";
+            }
+            if (errCounter == 0)
+            {
+                ErrorTBk.Text = "";
+                List<Employee> employees = DBConn.SEnt.Employee.ToList();
+                Employee currentEmployee = employees.FirstOrDefault(x => x.Login == login && x.Password == password);
+                if (currentEmployee != null)
+                {
+                    if (currentEmployee.Position == "зав. кафедрой")
+                        NavigationService.Navigate(new ZavCafPage(currentEmployee));
+                    else if (currentEmployee.Position == "преподаватель")
+                        NavigationService.Navigate(new TeacherPage(currentEmployee));
+                    else
+                        NavigationService.Navigate(new EngineerPage(currentEmployee));
+                }
+                else
+                {
+                    ErrorTBk.Text = "Неверный логин или пароль.";
+                }
+            }
+        }
+    }
+}
